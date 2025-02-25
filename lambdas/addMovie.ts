7817f32cb72664d89b293ef1eb1d28a6a01606a2
zft666zft <2810458566@qq.com>
2025-02-25 11:20:32 +0000

    Add API endpoint for posting a new movie.

---
+++ lambdas/addMovie.ts
@@ -0,0 +1,76 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import Ajv from "ajv";
+import schema from "../shared/types.schema.json";
+
+const ajv = new Ajv();
+const isValidBodyParams = ajv.compile(schema.definitions["Movie"] || {});
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  try {
+    // Print Event
+    console.log("[EVENT]", JSON.stringify(event));
+    const body = event.body ? JSON.parse(event.body) : undefined;
+    if (!body) {
+      return {
+        statusCode: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ message: "Missing request body" }),
+      };
+    }
+    if (!isValidBodyParams(body)) {
+        return {
+          statusCode: 500,
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            message: `Incorrect type. Must match Movie schema`,
+            schema: schema.definitions["Movie"],
+          }),
+        };
+      }
+
+    const commandOutput = await ddbDocClient.send(
+      new PutCommand({
+        TableName: process.env.TABLE_NAME,
+        Item: body,
+      })
+    );
+    return {
+      statusCode: 201,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ message: "Movie added" }),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -71,10 +71,23 @@ export class RestAPIStack extends cdk.Stack {
             resources: [moviesTable.tableArn],
           }),
         });
+
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
         
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
         
     // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
@@ -95,13 +108,16 @@ export class RestAPIStack extends cdk.Stack {
       "GET",
       new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
     );
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
 
     const movieEndpoint = moviesEndpoint.addResource("{movieId}");
     movieEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
     );
-
         
       }
     }
